78da0703e68e7df3a182a95c4f2ee07cc1c045cb
ajambure <abhishek.jambure98@gmail.com>
2018-10-16 18:50:35 +0530

    added new files

--- Crawler/src/com/webcrawler/utilities/SpiderUtility.java
+++ Crawler/src/com/webcrawler/utilities/SpiderUtility.java
@@ -1,6 +1,17 @@
-package com.webcrawler.utilities;;
+package com.webcrawler.utilities;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import jdk.nashorn.internal.runtime.regexp.joni.Regex;
+
 
 public class SpiderUtility {
+	//GETS STRING WITHIN DELIMTERS
 	public static String getStringWithinDelimiters(String pageSource,String startString,String endString)
 	{
 		if(pageSource == null  || pageSource.length() == 0)
@@ -8,59 +19,82 @@ public class SpiderUtility {
 
 		if(startString == null || startString.length() == 0 || endString == null || endString.length() == 0)
 			return "";
+		
 		int lengthofstartstring = startString.length();
 		int startStringIndex = pageSource.indexOf(startString);
 		int endStringIndex = pageSource.indexOf(endString);
 
-		String s = pageSource.substring(startStringIndex+lengthofstartstring,endStringIndex);
+		
 		//System.out.println(s);
+		if(endStringIndex < 0) {
+			return "";
+		}
+		String s = pageSource.substring(startStringIndex+lengthofstartstring,endStringIndex);
 		return s;
 
 	}
-
+//GETS PLAIN STRING WITHOUT HTML
 	static String getPlainStringwithouthtml(String inputString)
 	{
 		String noHtml = inputString.toString().replaceAll("<[^>]*>", "").replaceAll("\t","").replaceAll("\r","");
 		String Nospace = noHtml.trim();
-		System.out.println(Nospace);
+		//System.out.println(Nospace);
 		return Nospace;
 	}
-	
+//GETS PLAIN STRING WITHIN DELIMITER WITHOUT HTML
 	public static String getPlainStringWithinDelimiters(String pageSource,String startString, String endString) {
-		
+
 		String output = getStringWithinDelimiters(pageSource, startString, endString);
 		output = getPlainStringwithouthtml(output);
 		return output;
-		
-	}
-	
-	
-public static void main(String[] args) {
-	String inputString = getStringWithinDelimiters("p> </p>\r\n" + 
-			"  \r\n  " + 
-			"<div class=\"question\">\r\n" + 
-			"<h3>Questions 27–31</h3>\r\n" + 
-			"\r\n" + 
-			"<p>Reading passage 3 has six paragraphs, A–F.</p>\r\n" + 
-			"\r\n" + 
-			"<p>Which paragraph contains the following information?</p>\r\n" + 
-			"\r\n" + 
-			"<p> </p>\r\n" + 
-			"\r\n" + 
-			"<table border=\"0\" cellpadding=\"1\" cellspacing=\"1\"><tbody><tr><td width=\"800 px\">\r\n" + 
-			"			<h3>Questions</h3>\r\n" + 
-			"			</td>\r\n" + 
-			"		</tr><tr><td>27) a use for helium which makes an activity safer</td>\r\n" + 
-			"		</tr><tr><td>28) the possibility of creating an alternative to helium</td>\r\n" + 
-			"		</tr><tr><td>29) a term which describes the process of how helium is taken out of the ground</td>\r\n" + 
-			"		</tr><tr><td>30) a reason why users of helium do not make efforts to conserve it</td>\r\n" + 
-			"		</tr><tr><td>31) a contrast between helium’s chemical properties and how non-scientists think about it</td>\r\n" + 
-			"		</tr></tbody></table></div>\r\n" + 
-			"\r\n" + 
-			"<p> </p>","Reading","why");	
-	//getPlainString(inputString);
 
-	System.out.println(inputString); 
 	}
+//GETS ALL THE STRINGS BETWEEN DELIMITERS FROM A PAGE SOURCE AND RETURNS A LIST OF THEM
+	public static List<String> getListwithinDelimiter(String pageSource,String startString, String endString){
+		
+		if(startString == null || startString.length() == 0 || endString == null || endString.length() == 0)
+		return Collections.emptyList();	
+		
+		
+		ArrayList<String> ls = new ArrayList<String>(); 
+		Pattern pattern = Pattern.compile(startString);
+		Matcher matcher = pattern.matcher(pageSource);
+		
+		while(matcher.find()) {
+			
+			int startindex = matcher.start();
+			String s = pageSource.substring(startindex);
+			String ss = getStringWithinDelimiters(s, startString, endString);
+			
+			ls.add(ss);
+		}
+		//System.out.println(ls);
+			
+		return ls;
+
+
+
+	}
+	//GETS PLAIN LIST WITHIN DELIMITER WITHOUT HTML
 	
+	public static List<String> getPlainListWithinDelimiters(String pageSource,String startString, String endString){
+		
+		List<String> ls = getListwithinDelimiter(pageSource, startString, endString);
+		ls = ls.stream().map(element -> getPlainStringwithouthtml(element)).collect(Collectors.toList());
+		System.out.println(ls);
+		return ls;		
+	}
+
+	public static void main(String[] args) {
+		getStringWithinDelimiters("<!DOCTYPE html>\r\n" + 
+				"<html>\r\n" + 
+				"<body>\r\n" + 
+				"\r\n" + 
+				"<p>This is a paragraph.</p>\r\n" + 
+				"<p>This is another paragraph.</p>\r\n" + 
+				"\r\n" + 
+				"</body>\r\n" + 
+				"</html>","" , "");
+	}
+
 }
